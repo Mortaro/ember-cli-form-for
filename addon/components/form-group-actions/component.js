08c71b026979bdc009f65b8475a93939844d3462
Barneche <harerako@gmail.com>
2018-01-19 09:18:38 -0200

    improving templates

--- addon/components/form-group-actions/component.js
+++ addon/components/form-group-actions/component.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 import layout from './template';
 
 export default Ember.Component.extend({
-  
+
   layout,
 
-  classNames: ['col-md-12']
+  classNames: ['col-md-12'],
+
+  label: 'Salvar'
 
 });
